cf3ebe2f9a1393f320598330e06a3e1fc42ef143
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2026-05-06 10:10:49 -0300

    Exercício de Fixação 02, do laboratório 05

--- Lab05/ef02.cpp
+++ Lab05/ef02.cpp
@@ -1,7 +1,26 @@
 #include <iostream>
 using namespace std;
 
+float convertCtoF(float Celsius);
+
 int main() {
+    float Celsius, Fahrenheit = 0.0;
+
+    cout << "Digite uma temperatura em graus Celsius: ";
+    cin >> Celsius;
+    cout << endl;
+
+    Fahrenheit = convertCtoF(Celsius);
+
+    cout << Celsius << " graus Celsius equivalem a " << Fahrenheit << " graus Fahrenheit." << endl;
 
     return 0;
+}
+
+float convertCtoF(float Celsius){
+    float Farebheut = 0;
+
+    Farebheut = 1.8 * Celsius + 32.0;
+
+    return Farebheut;
 }
