952a6e0471a581451c023a7fb0b97b6380242ac0
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2026-05-14 10:07:00 -0300

    Exercício de fixação 05

---
+++ Lab05/ef05.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int x, y = 0;
+    float norma = 0.0;
+
+    cout << "Digite as coordenadas do vetor" << endl;
+    
+    cout << "x: ";
+    cin >> x;
+
+    cout << "y: ";
+    cin >> y;
+
+    return 0;
+}
+
+!@Sucupira314
