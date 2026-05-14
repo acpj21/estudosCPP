9a26aa0651e79bbdb827c6c15b549f10a4d977b0
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2026-05-14 13:57:06 -0300

    Exercício de fixação 01, do laboratório 06

---
+++ Lab06/ef01.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int total;
+
+    cout << "Inicialmente total = " << total << " (ZERO)" << endl;
+
+    total = 50;
+
+    cout << "Depois da atribuição total = " << total << endl;
+
+    return 0;
+}
