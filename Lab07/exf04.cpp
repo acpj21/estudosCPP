afbbc4c85bf7060dcef50df17a74fb34f6c5fa0f
Antonio Carlos Peron Júnior <antonio.peron@cmc.pr.gov.br>
2026-06-16 15:31:38 -0300

    Exerício de fixação 04 do laboratorio 07

---
+++ Lab07/exf04.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Maçã = \t\t 20 unidades" << endl;
+    cout << "Melancia = \t 50 unidades" << endl;
+    cout << "Ameixa = \t 30 unidades" << endl;
+
+    return 0;
+}
